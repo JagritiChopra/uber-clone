2c3aeef38e2bf1036e6f5afc2261c6b3243afc95
JagritiChopra <jagritichopra18@gmail.com>
2025-12-18 03:42:29 -0800

    completed

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,21 @@
 // https://docs.expo.dev/guides/using-eslint/
 const { defineConfig } = require('eslint/config');
 const expoConfig = require('eslint-config-expo/flat');
+const eslintPluginPrettierRecommended = require('eslint-plugin-prettier/recommended');
 
 module.exports = defineConfig([
+  globalIgnores(['dist/*']),
   expoConfig,
+  { 
+    
+    ignores: ['dist/*'],
+    files: ['babel.config.js'],
+    languageOptions: {
+      globals: globals.node,} ,
+  },
+    eslintPluginPrettierRecommended,
   {
     ignores: ['dist/*'],
   },
+
 ]);
